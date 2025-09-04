c1e2a738ab4998bc7e0c7c759beffb109fef0c5a
caioagrelli <caiooagrelli@hotmail.com>
2025-09-04 17:09:58 -0300

    mudei as pastas

---
+++ backend/usuarios/urls.py
@@ -0,0 +1,12 @@
+# usuarios/urls.py
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('login/', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('administrativo/', views.pagina_administrativa, name='pagina_administrativa'),
+]
--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required # Ferramenta de segurança do Django
 
-# Create your views here.
+# View para a Homepage
+def homepage(request):
+    return render(request, 'usuarios/homepage.html')
+
+# View para a página administrativa
+@login_required # <-- ESTE É O "SEGURANÇA" NA PORTA
+def pagina_administrativa(request):
+    # Primeiro, crie o arquivo HTML para esta página
+    # em 'backend/usuarios/templates/usuarios/pagina_administrativa.html'
+    return render(request, 'usuarios/pagina_administrativa.html')
